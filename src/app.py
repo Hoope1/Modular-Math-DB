8a360455c7595da0381e5edec13bbb2a6ded0685
Hoope1 <109686102+Hoope1@users.noreply.github.com>
2024-11-30 18:29:12 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 import pandas as pd
 from src.components.ui_elements import render_header, render_footer
